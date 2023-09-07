0808af97d9861814d135f1f5afcb5a6135dd2a00
RedLife <devilder.d@gmail.com>
2023-09-07 09:22:20 +0200

    07-09-23

---
+++ DB/Procedures/Dlc/ReadAllDlcLight.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[SP_Dlc_ReadAllDlcLight]
+    @IdCust int 
+AS
+BEGIN
+	/*
+        Test de valeurs
+    */
+	SELECT id,DtIn
+	FROM [Dlc]
+	WHERE IdCustomer = @IdCust
+	RETURN 0;
+END
